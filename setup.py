52249650464a033a2f19e6a3081e969cb37142bb
Elizabeth Sall <easall@gmail.com>
2020-06-16 09:55:40 -0700

    bump version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ install_requires = [r.strip() for r in requirements]
 
 setup(
     name="gmnspy",
-    version="0.0.1",
+    version="0.0.2",
     description="",
     long_description=long_description,
     long_description_content_type="text/markdown",
