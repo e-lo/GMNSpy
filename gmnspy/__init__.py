e5a689d23679031a17d8f6e8f59f85b61b2b50c8
Elizabeth Sall <easall@gmail.com>
2023-06-09 10:40:24 -0700

    try explicit module listing

--- gmnspy/__init__.py
+++ gmnspy/__init__.py
@@ -7,6 +7,21 @@ Typical usage:
     ```
 """
 
-from .in_out import *
-from .schema import *
+from .in_out import read_gmns_csv, read_gmns_network
+from .schema import (
+    document_schemas_to_md,
+    document_spec_to_md,
+    read_config,
+    read_schema,
+)
 from .utils import list_to_md_table, logger
+
+__all__ = [
+    "read_gmns_csv",
+    "read_gmns_network",
+    "document_schemas_to_md",
+    "document_spec_to_md",
+    "read_config," "read_schema",
+    "list_to_md_table",
+    "logger",
+]
