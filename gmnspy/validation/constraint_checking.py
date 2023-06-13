21078114e310ea85424593437a17e26839f1afd0
Elizabeth Sall <easall@gmail.com>
2023-06-12 17:01:29 -0700

    Add constraint tests
    And refactor constraint checking

--- gmnspy/validation/constraint_checking.py
+++ gmnspy/validation/constraint_checking.py
@@ -13,34 +13,33 @@ from typing import Union
 
 import pandas as pd
 
+from gmnspy.utils import logger
+
 
 def _required_constraint(_s, _p) -> Union[None, str]:
     """Placeholder for something tested somewhere else."""
     pass
 
 
-def _unique_constraint(s: pd.Series, _) -> Union[None, str]:
+def _unique_constraint(s: pd.Series) -> Union[None, str]:
     """
     Check if series contains unique values.
 
-    TODO: ##needstest
-
     Args:
-        s: series that shouldn't exceed maximum value.
-        _: boolean specifying unique values needed.
+        s: series to test
 
     Returns:
         An error string if there is an error. Otherwise, None.
     """
-    if s.dropna().duplicated():
-        return "Values not unique."
-
+    _dupes = s[s.duplicated(keep="first")]
+    if len(_dupes):
+        msg = f"{len(_dupes)} sets of duplicate values not unique. Duplicate values: {_dupes.to_list()}"
+        logger.warning(msg)
+        return msg
 
 def _minimum_constraint(s: pd.Series, minimum: Union[float, int]) -> Union[None, str]:
     """
     Check if series contains value under the specified minimum.
-
-    TODO: ##needstest
 
     Args:
         s: series that shouldn't be under the minimum value.
@@ -49,15 +48,15 @@ def _minimum_constraint(s: pd.Series, minimum: Union[float, int]) -> Union[None,
         Returns:
         An error string if there is an error. Otherwise, None.
     """
-    if s[s < minimum].dropna().to_list():
-        return "Values lower than minimum: {}".format(minimum)
-
-
+    _bad_vals = s[s < minimum].dropna().to_list()
+    if _bad_vals:
+        msg = f"{len(_bad_vals)} values lower than minimum value {minimum}: {_bad_vals}"
+        logger.warning(msg)
+        return msg
+    
 def _maximum_constraint(s: pd.Series, maximum: Union[float, int]) -> Union[None, str]:
     """
     Check if series contains value above the specified maximum.
-
-    TODO: ##needstest
 
     Args:
         s: series that shouldn't exceed maximum value.
@@ -66,15 +65,15 @@ def _maximum_constraint(s: pd.Series, maximum: Union[float, int]) -> Union[None,
     Returns:
         An error string if there is an error. Otherwise, None.
     """
-    if s[s > maximum].dropna().to_list():
-        return "Values higher than maximum: {}".format(maximum)
-
+    _bad_vals = s[s > maximum].dropna().to_list()
+    if _bad_vals:
+        msg = f"{len(_bad_vals)} values higher than maximum value {maximum}: {_bad_vals}"
+        logger.warning(msg)
+        return msg
 
 def _pattern_constraint(s: pd.Series, pattern: str) -> Union[None, str]:
     """
     Check if series contains values conforming to specified pattern.
-
-    TODO: ##needstest
 
     Args:
         s: series that shouldn't be under the minimum value.
@@ -83,15 +82,16 @@ def _pattern_constraint(s: pd.Series, pattern: str) -> Union[None, str]:
     Returns:
         An error string if there is an error. Otherwise, None.
     """
-    if ~s.str.contains(pattern):
-        return "Doesn't match pattern: {}".format(pattern)
+    notmatching_s = s.loc[~s.str.fullmatch(pattern)]
+    if len(notmatching_s):
+        msg = f"{len(notmatching_s)} values don't match pattern: {pattern}: {notmatching_s.to_list()}"
+        logger.warning(msg)
+        return msg
 
 
 def _enum_constraint(s: pd.Series, enum: Union[str, list], sep: str = ",") -> Union[None, str]:
     """
     Check if series contains valid enum values.
-
-    TODO: ##needstest
 
     Args:
         s: series of values that should all have values in  the enumerated
@@ -105,6 +105,8 @@ def _enum_constraint(s: pd.Series, enum: Union[str, list], sep: str = ",") -> Un
     """
     if not isinstance(enum, list):
         enum = enum.split(sep)
-    err_i = (s[~s.isin(enum)]).to_list()
-    if err_i:
-        return "Values: {} not in enumerated list: {}".format(err_i, enum)
+    bad_vals = (s[~s.isin(enum)]).to_list()
+    if bad_vals:
+        msg = f"Values: {bad_vals} not in enumerated list: {enum}"
+        logger.warning(msg)
+        return msg
